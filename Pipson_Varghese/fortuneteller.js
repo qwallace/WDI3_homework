eaa9d7e30fb91138f1bca94f7558e5a98fe8780a
pipson <pipsonvarghese@gmail.com>
2015-07-01 05:48:45 +1000

    fortuneteller entered

---
+++ Pipson_Varghese/fortuneteller.js
@@ -0,0 +1,12 @@
+function fortuneteller(){
+
+	var children,parents,geoLocation,jobTitle;
+
+	children = 5;
+	partner = "cindy";
+	geoLocation = "Narre Warren";
+	jobTitle = " Software Eng.";
+
+	console.log("You will be a "+ jobTitle + "in  2yr , and married to "+ partner + " ,"+geoLocation + "  with "+ children + " kids.");
+	
+}
